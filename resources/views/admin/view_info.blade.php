ac8ff978dd3b7d84250c5566b5f584bc02172246
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-10 18:06:21 +0200

    Update view_info.blade.php

--- resources/views/admin/view_info.blade.php
+++ resources/views/admin/view_info.blade.php
@@ -6,17 +6,17 @@
             <h2>edit movie info</h2>
         </div>
         <div class="card-body">
-        <form action="{{ url('admin/view_info/'  . $movies->id ) }}"  enctype="multipart/form-data" method="post">
+        <form action="{{ url('admin/view_info/'  . $movie->id ) }}"  enctype="multipart/form-data" method="post">
             @csrf
             {{ method_field('PATCH') }}
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="{{ $movies->name }}">
+                <input type="text" name="name" class="form-control" value="{{ $movie->name }}">
 
                 <label>satus</label>
                 <select name="status" class="form-control">
                 <option value="">--select</option>
-                <option value="coming soon" @if ($movies->status == 'coming soon') selected @endif>coming soon</option>
-                <option value="new movie"@if ($movies->status == 'new movie') selected @endif>  new movie</option>
+                <option value="coming soon" @if ($movie->status == 'coming soon') selected @endif>coming soon</option>
+                <option value="new movie"@if ($movie->status == 'new movie') selected @endif>  new movie</option>
                 </select>
 
                 <label>Image</label>
@@ -24,7 +24,7 @@
                 <input type="file" name="image" class="form-control">
 
                 <label>Description</label>
-                <textarea name="description" class="form-control">{{ $movies->description }}</textarea>
+                <textarea name="description" class="form-control">{{ $movie->description }}</textarea>
 
                 <button class="btn btn-primary" type="submit" id="edit1" >edit</button>
             </form>
