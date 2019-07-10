f5aba58d0f7375d3a4c58d8f4c398a1e54eef14a
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-10 18:06:27 +0200

    Update movies_page.blade.php

--- resources/views/movies_page.blade.php
+++ resources/views/movies_page.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.master')
 @section('content')
 <div class="card mb-4">
-<img src=" {{ url('storage/'.$movies->img) }}  " alt="" class="card-img-top">
+<img src=" {{ url('storage/'.$movie->img) }}  " alt="" class="card-img-top">
 <div class="card-body">
-<h3 class="card-title">{{ $movies->name }}</h3>
-    <p class="card-text text-muted">{{ $movies->status }}</p><br>
-    <p>rating</p> <a class="btn btn-primary" href="{{ url('book_tickets/' . $movies->id )}} " role="button">book a ticket</a>
+<h3 class="card-title">{{ $movie->name }}</h3>
+    <p class="card-text text-muted">{{ $movie->status }}</p><br>
+    <p>rating</p> <a class="btn btn-primary" href="{{ url('book_tickets/' . $movie->id )}} " role="button">book a ticket</a>
 </div>
 
 <div class="card-footer">
-<p>{{ $movies->description }}</p>
+<p>{{ $movie->description }}</p>
 </div>
 </div>
 @endsection
