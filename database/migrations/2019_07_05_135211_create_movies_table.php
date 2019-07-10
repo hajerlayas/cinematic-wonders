b3a1915f8cf8d093c386ed918f6b042e4200d0a8
OlaKhoja <olakhoja21@gmail.com>
2019-07-10 17:34:07 +0200

    Update 2019_07_05_135211_create_movies_table.php

--- database/migrations/2019_07_05_135211_create_movies_table.php
+++ database/migrations/2019_07_05_135211_create_movies_table.php
@@ -16,7 +16,7 @@ class CreateMoviesTable extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('img');
             $table->string('status');
             
