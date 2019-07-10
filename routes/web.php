0e6bff7c404d567d061ea3435a505392c77ce6b9
OlaKhoja <olakhoja21@gmail.com>
2019-07-10 17:35:26 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/layout', function () {
+Route::get('/', function () {
     return view('layout.master');
 });
+Route::get('movies/create', 'movieController@create');
+Route::post('movies'      , 'movieController@store' );
+
+Route::get('movies', 'movieController@index'); //this will list the movies
+Route::get('movies/{movie_id}/times/create', 'MovieTimeController@create');
+Route::post('movies/{movie_id}/times', 'MovieTimeController@store');
+
+Route::get('customer/view', 'customerController@show');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
