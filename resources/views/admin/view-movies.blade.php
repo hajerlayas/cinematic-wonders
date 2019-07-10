f69f8c44ef2944e7e93998aae2861b9841d195cc
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-10 17:41:45 +0200

    Create view-movies.blade.php

---
+++ resources/views/admin/view-movies.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.master')
+@section('content')
+
+<div class="row">
+    @foreach($movies as $movie)
+        <div class="col-xl-3 col-lg-6">
+            <div class="card mb-4">
+
+                <img src=" {{ url('storage/'.$movie->img) }} " alt="" class="card-img-top">
+
+                <div class="card-body">
+                <h3 class="card-title">{{ $movie->name }}</h3>
+                    <p class="card-text text-muted">{{ $movie->type }}</p>
+                </div>
+
+                <div class="card-footer">
+                <a class="btn btn-primary" href="#" role="button">show more</a>
+                <a class="btn btn-primary" href="{{ url('admin/view_info/' . $movie->id )}} " role="button">edit</a>
+                </div>
+            </div>
+          </div>
+    @endforeach
+</div>
+
+@endsection
