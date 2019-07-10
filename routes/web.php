6d1ce398e0d96992b3e9f4d77de8ba34c8b5f71a
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-10 17:42:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/layout', function () {
     return view('layout.master');
 });
+Route::get('admin','view_moviesController@view');
+Route::get('admin/view_info/{id}','view_moviesController@get_info');
+Route::patch('admin/view_info/{id}', 'view_moviesController@update');
+Route::get('movies_page/{id}','view_moviesController@index');
+Route::get('movies','view_moviesController@movies');
+Route::get('book_tickets/{id}','view_moviesController@book_tickets');
