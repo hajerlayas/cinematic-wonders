3bf3c2e91f7a7b8a93bddd2b07f761c951aaffb5
OlaKhoja <olakhoja21@gmail.com>
2019-07-10 17:33:43 +0200

    Create MovieTimeController.php

---
+++ app/Http/Controllers/MovieTimeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MovieTimeController extends Controller
+{
+    public function create($movie_id)
+    {
+        $movie = \App\Movie::find($movie_id);
+
+        return view('movies.times.create')->with('movie', $movie);
+    }
+
+    public function store($movie_id)
+    {
+        $new_time = new \App\Time;
+        $new_time->date = request()->date;
+        $new_time->start = request()->start;
+        $new_time->end = request()->end;
+        $new_time->movie_id = $movie_id;
+        
+        $new_time->save();
+
+        return redirect('/movies');
+    }
+}
