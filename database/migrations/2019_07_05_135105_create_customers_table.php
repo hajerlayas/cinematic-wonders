89f236ae37f60ae5570564505586e0c57d5b5cf2
OlaKhoja <olakhoja21@gmail.com>
2019-07-11 14:37:09 +0200

    Create 2019_07_05_135105_create_customers_table.php

---
+++ database/migrations/2019_07_05_135105_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('number');
+            $table->date('day');
+            $table->time('start');
+            $table->bigInteger('seates');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
