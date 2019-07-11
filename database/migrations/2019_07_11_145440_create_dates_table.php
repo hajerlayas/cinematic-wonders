e5c31f79839f26aa525adf05bd23c92580667804
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-11 17:42:18 +0200

    Create 2019_07_11_145440_create_dates_table.php

---
+++ database/migrations/2019_07_11_145440_create_dates_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dates', function (Blueprint $table) {
+            //times table
+            $table->bigIncrements('id');
+            $table->bigInteger('time_id');
+            $table->bigInteger('movie_id');
+            $table->time('start');
+            $table->time('end');
+
+            
+
+            $table->timestamps();
+        });
+
+        Schema::table('dates', function (Blueprint $table) {
+            $table->foreign('movie_id')
+            ->references('id')->on('movies')
+            ->onDelete('cascade');
+        });
+
+        Schema::table('dates', function (Blueprint $table) {
+            $table->foreign('time_id')
+            ->references('id')->on('times')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dates');
+    }
+}
