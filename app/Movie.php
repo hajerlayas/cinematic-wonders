c1e3e7f66717e1ffd2402e8d6812cc7d2d687267
OlaKhoja <olakhoja21@gmail.com>
2019-07-11 11:22:29 +0200

    Update Movie.php

--- app/Movie.php
+++ app/Movie.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    public function times(){
+    public function times()
+    {
         return $this->belongsToMany('App\Time');
+    }
+
+    
+    public function Catagories()
+    {
+        return $this->belongsToMany('App\Catagories');
     }
 }
