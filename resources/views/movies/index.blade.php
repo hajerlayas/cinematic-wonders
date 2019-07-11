e568c752158a5e72abfcb1cb4b6dbca58d199b41
OlaKhoja <olakhoja21@gmail.com>
2019-07-11 11:22:18 +0200

    Delete index.blade.php

--- resources/views/movies/index.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach ($movies as $movie)
-
-<p>
-{{ $movie->name }}
-<img height="200" src="{{ url('storage/' . $movie->img) }}">
-<a href="{{ url("movies/{$movie->id}/times/create") }}">Add Time</a>
-</p>
-
-@endforeach
