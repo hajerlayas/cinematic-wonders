f765335254144fec0598c0eb3c34983b3e5f5d5e
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-12 13:34:29 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('admin','view_moviesController@view');
 Route::get('movies_page/{id}','view_moviesController@index');
 Route::get('show_movies','view_moviesController@movies');
 Route::get('book_tickets/{id}','view_moviesController@book_tickets');
+Route::get('index','view_moviesController@movies');
+Route::post('contactus','view_moviesController@contactus');
 Route::post('book','view_moviesController@store');
 Route::post('movies/{movie_id}/times', 'MovieTimeController@store');
 Route::post('movies'      , 'movieController@store' );
