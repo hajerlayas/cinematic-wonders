6ac7618b594669a741386b32e3fc601d2f8a5bb2
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-13 19:08:13 +0200

    Update movies_page.blade.php

--- resources/views/movies_page.blade.php
+++ resources/views/movies_page.blade.php
@@ -1,7 +1,15 @@
 @extends('layout.master')
+<head>
+    <style>
+    .card{
+            width:750;
+            height:600;
+        }
+    </style>
+</head>
 @section('content')
-<div class="card mb-4">
-<img src=" {{ url('storage/'.$movie->img) }}  " alt="" class="card-img-top">
+<div class="card mb-4 mx-auto">
+<img src=" {{ url('storage/'.$movie->img) }}  " width="300" height="300" alt="" class="card-img-top">
 <div class="card-body">
 <h3 class="card-title">{{ $movie->name }}</h3>
     <p class="card-text text-muted">{{ $movie->status }}</p><br>
