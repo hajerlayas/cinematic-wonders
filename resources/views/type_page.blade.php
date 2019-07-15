80fb95fc5cbbdb851a6db90654829074298fecd7
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-07-15 20:36:07 +0200

    Update type_page.blade.php

--- resources/views/type_page.blade.php
+++ resources/views/type_page.blade.php
@@ -9,16 +9,16 @@
 </head>
 @section('content')
 <div class="card mb-4 mx-auto">
-<img src=" {{ url('storage/'.$movie->img) }}  " width="300" height="300" alt="" class="card-img-top">
+<img src=" {{ url('storage/'.$movies->img) }}  " width="300" height="300" alt="" class="card-img-top">
 <div class="card-body">
-<h3 class="card-title">{{ $movie->name }}</h3>
-    <p class="card-text text-muted">{{ $movie->status }}</p><br>
+<h3 class="card-title">{{ $movies->name }}</h3>
+    <p class="card-text text-muted">{{ $movies->status }}</p><br>
     <p>rating</p> 
-    <a class="btn btn-primary" href="{{ url('book_tickets/' . $movie->id )}} " role="button">book a ticket</a>
+    <a class="btn btn-primary" href="{{ url('book_tickets/' . $movies->id )}} " role="button">book a ticket</a>
 </div>
 
 <div class="card-footer">
-<p>{{ $movie->description }}</p>
+<p>{{ $movies->description }}</p>
 </div>
 </div>
 @endsection
