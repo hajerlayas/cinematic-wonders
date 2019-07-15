56cf52dd3261eeb15eb5fcecd6c404a34590c2be
OlaKhoja <olakhoja21@gmail.com>
2019-07-15 20:37:29 +0200

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -87,6 +87,10 @@
                             <a href="{{ url('customer/view') }}" class="nav-link">customer details</a>
                             </li>
 
+                            <li class="nav-item">
+                            <a href="{{ url('admin/message') }}" class="nav-link">messages</a>
+                            </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
