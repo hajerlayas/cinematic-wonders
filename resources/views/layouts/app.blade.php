23f8cb739b6de714a0da944897ea57b46fa78413
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-08-15 14:48:21 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<style>
+.navbarDropdown{
+    float: right;
+}
+</style>
+
 </head>
 <body>
     <div id="app">
