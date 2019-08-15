eca05d4b4f3d05dea0eb269fbd9137a683362146
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-08-15 14:48:15 +0200

    Update view-movies.blade.php

--- resources/views/admin/view-movies.blade.php
+++ resources/views/admin/view-movies.blade.php
@@ -4,9 +4,9 @@
 <div class="row">
     @foreach($movie as $movie)
         <div class="col-xl-3 col-lg-6">
-            <div class="card mb-4">
+            <div class="card mb-4 mt-5">
 
-                <img src=" {{ url('storage/'.$movie->img) }} " alt="" class="card-img-top">
+                <img src=" {{ url('storage/'.$movie->img) }} " width=" 300px" height="200px" alt=""  class="card-img-top">
 
                 <div class="card-body">
                 <h3 class="card-title">{{ $movie->name }}</h3>
