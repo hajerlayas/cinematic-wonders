16afa4ba9872fc9796dab9e69f721ecec7928fb4
hajerlayas <46102907+hajerlayas@users.noreply.github.com>
2019-08-15 14:48:26 +0200

    Update type_page.blade.php

--- resources/views/type_page.blade.php
+++ resources/views/type_page.blade.php
@@ -1,24 +1,30 @@
 @extends('layout.master')
-<head>
-    <style>
-    .card{
-            width:750;
-            height:600;
-        }
-    </style>
-</head>
+
+<style>
+footer{
+     position: fixed !important;
+            }
+</style>
 @section('content')
-<div class="card mb-4 mx-auto">
-<img src=" {{ url('storage/'.$movies->img) }}  " width="300" height="300" alt="" class="card-img-top">
-<div class="card-body">
-<h3 class="card-title">{{ $movies->name }}</h3>
-    <p class="card-text text-muted">{{ $movies->status }}</p><br>
-    <p>rating</p> 
-    <a class="btn btn-primary" href="{{ url('book_tickets/' . $movies->id )}} " role="button">book a ticket</a>
+
+<div class="row">
+    @foreach($movies as $movie)
+        <div class="col-xl-2 col-lg-6">
+            <div class="card mb-4">
+
+                <img src=" {{ url('storage/'.$movie->img) }}  " width=" 300px" height="100px" alt="" class="card-img-top">
+
+                <div class="card-body">
+                <h3 class="card-title">{{ $movie->name }}</h3>
+                    <p class="card-text text-muted">{{ $movie->type }}</p>
+                </div>
+
+                <div class="card-footer">
+                <a class="btn btn-primary" href="{{ url('movies_page/' . $movie->id )}}" role="button">show more</a>
+                </div>
+            </div>
+          </div>
+    @endforeach
 </div>
 
-<div class="card-footer">
-<p>{{ $movies->description }}</p>
-</div>
-</div>
 @endsection
